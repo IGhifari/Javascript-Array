657deabde6e1e0d5431c59597524c78cbf571df3
IGhifari <rinnitoshii07@gmail.com>
2025-03-14 19:06:51 +0700

    array key

---
+++ arraykey.js
@@ -0,0 +1,16 @@
+// let barbel = [];
+
+// barbel.push(10);
+// barbel.push(20);
+// barbel.push(30);
+// console.log(barbel.pop());
+// console.log(barbel.shift());
+// barbel.unshift(40);
+// barbel.unshift(50);
+// console.log(barbel);
+
+let students = ["Ghifari"];
+students.unshift("Rizky");
+students.unshift(0)
+students.shift();
+console.log(students);
