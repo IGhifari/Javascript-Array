10367a33193d7850432ce6b7d407d8b14c222ead
IGhifari <rinnitoshii07@gmail.com>
2025-03-17 19:37:11 +0700

    method array

---
+++ methodArray.js
@@ -0,0 +1,16 @@
+// const Array1 = [1, 2, 3, 4, 5];
+// const Array2 = [6, 7, 8, 9, 10];
+
+// console.log(Array1.concat(Array2));
+
+const animales = ["Lion", "Tiger", "Elephant", "Zebra", "Giraffe", "Tiger"];
+
+// console.log(animales.indexOf("Tiger"));
+
+// console.log(animales.includes("Lion"));
+
+// console.log(animales.sort())
+
+console.log(animales.splice(1, 0, "Monkey"));
+
+console.log(animales);
